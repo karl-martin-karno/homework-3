4bebf9447991836b7f205255c97a3b44d4afe251
birgittmajas <birgitt.majas@gmail.com>
2020-11-20 19:48:20 +0200

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,22 @@
 <template>
   <div id="app">
     <Header />
-   
-    
-
+    <router-view/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 
-
 export default {
   name: 'App',
   components: {
-  
+
     Header
   }
 }
 </script>
 
 <style>
-@import './assets/style.css'
+@import './assets/style.css';
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,30 +1,29 @@
 <template>
-<nav>
-            <div class="logo-container">
-                <img src="@/assets/logo.png" alt="postIt">
-            </div>
-            <div class="search-container">
-                <input type="text" name="search"><button type="button">Search</button>
-            </div>
-            <div class="avatar-container">
-                <img id="avatar-img" src="@/assets/avatar.png" class="avatar" alt="Me">
-                <div class="dropdown">
-                    <ul>
-                        <li id="name"></li>
-                        <li id="email"></li>
-                        <hr>
-                        <li><a id="avatar-browse" href="browse.html">Browse</a></li>
-                        <li><a id="avatar-login" href="login.html">Log out</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    
+  <nav>
+    <div class="logo-container">
+      <img src="@/assets/logo.png" alt="postIt">
+    </div>
+    <div class="search-container">
+      <input type="text" name="search"><button type="button">Search</button>
+    </div>
+    <div class="avatar-container">
+      <img id="avatar-img" src="@/assets/avatar.png" class="avatar" alt="Me">
+      <div class="dropdown">
+        <ul>
+          <li id="name"></li>
+          <li id="email"></li>
+          <hr>
+          <li><a id="avatar-browse" href="browse.html">Browse</a></li>
+          <li><a id="avatar-login" href="login.html">Log out</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-    
+
 }
 </script>
 
